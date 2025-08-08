40b5c63cc87919277e5f741616786d35b34046f2
Jose Andres <andres.henao9@udea.edu.co>
2025-08-07 21:03:59 -0500

    The sequence has been done, which is the first step.
    Signed-off-by: Jose Andres <andres.henao9@udea.edu.co>

---
+++ secuencia.h
@@ -0,0 +1,14 @@
+#ifndef SECUENCIA_H
+#define SECUENCIA_H
+//definition Leds.
+#include <stdint.h>
+#define Led1 18
+#define Led2 19 
+#define Led3 20
+
+
+#define Mask ((1u << 18) | (1u << 19) | (1u << 20))
+void LedsSequence();
+void wait(uint16_t time_sleep);
+
+#endif
